59c066fcca7f9ba9777d4ede07f9019740c5e37b
Aman Shastry <73488475+Aman-Amith-Shastry@users.noreply.github.com>
2026-03-15 10:18:11 -0400

    Change base URL for Vite configuration

--- stuff/vite.config.js
+++ stuff/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Aman-Amith-Shastry/campusai-hackathon'
+  base: '/campusai-hackathon/'
 })
